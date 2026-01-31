19ad4162eeff349b33d136ffa481a9ab2aade00f
mohamed-hossam637 <mohamedhosamabdelhamed@gmail.com>
2026-01-31 15:31:04 +0200

    refactor : add skeleton loading for faqs section

--- resources/views/components/lazy-faq.blade.php
+++ resources/views/components/lazy-faq.blade.php
@@ -1,4 +1,5 @@
-<div class="rounded-lg px-4 md:px-[50px] pt-[30px] border border-gray-100 mb-5 bg-white flex items-center justify-between">
+<div
+    class="rounded-lg px-4 md:px-[50px] py-[15px] border border-gray-100 mb-5 bg-white flex items-center justify-between">
 
     <!-- Text Skeleton -->
     <div class="w-full me-5 space-y-3">
@@ -6,7 +7,13 @@
         <div class="h-4 w-2/3 rounded bg-gray-200 animate-pulse"></div>
     </div>
 
-     <!-- Image Skeleton -->
-    <div class="h-10 w-10 rounded-lg bg-gray-200 animate-pulse"></div>
+    <!-- Image Skeleton -->
+    <div class="h-10 w-10 rounded-lg bg-gray-200 animate-pulse flex items-center justify-center">
+        <svg class="fill-gray-400 w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" x-bind:class="{ 'rotate-180': expanded }"
+            class="transition-all duration-500 ease-in-out">
+            <path
+                d="M297.4 470.6C309.9 483.1 330.2 483.1 342.7 470.6L534.7 278.6C547.2 266.1 547.2 245.8 534.7 233.3C522.2 220.8 501.9 220.8 489.4 233.3L320 402.7L150.6 233.4C138.1 220.9 117.8 220.9 105.3 233.4C92.8 245.9 92.8 266.2 105.3 278.7L297.3 470.7z" />
+        </svg>
+    </div>
 
 </div>
